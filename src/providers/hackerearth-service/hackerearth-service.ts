97390882f0d8358d5c62bb6f0d92380e50ccf7c0
Terry Palomares <tpalomares18@gmail.com>
2017-10-29 18:37:37 -1000

    hackerearth runcode provider created

---
+++ src/providers/hackerearth-service/hackerearth-service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class HackerEarthServiceProvider {
+
+  private BASE_URL = 'http://api.hackerearth.com/code/';
+  private CLIENT_SECRET = '3a61e309ecccd4f3cdb35d58e29115129d68a907';
+
+  constructor(public http: Http) { }
+
+  /**
+   * Runs a given code snippet
+   * @method runSnippet
+   * @param language programming language
+   * @param snippet actual code to run
+   * @return promise with response
+   */
+  runSnippet(language, snippet) {
+    const body = {
+    'client_secret': this.CLIENT_SECRET,
+    'async': 0,
+    'source': snippet,
+    'lang': language,
+    'time_limit': 5,
+    'memory_limit': 262144,
+    }
+
+    return this.http.post(this.BASE_URL + '/run/', body)
+  }
+
+}
