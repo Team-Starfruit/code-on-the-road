bcce62d25ee93e4254883cd7c072ea2e8832e0df
Terry Palomares <tpalomares18@gmail.com>
2017-11-02 01:00:21 -1000

    added snippets component

---
+++ src/pages/snippets/snippets.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-snippets',
+  templateUrl: 'snippets.html',
+})
+
+
+export class SnippetsPage {
+
+  constructor(public navCtrl: NavController) {}
+
+}
