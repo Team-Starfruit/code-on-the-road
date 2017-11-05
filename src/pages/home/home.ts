7304ec8956ce4e69543992c52cb3e1e266b4b3fd
Terry Palomares <tpalomares18@gmail.com>
2017-11-04 20:32:11 -1000

    Updated home to not include snippet editor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,29 +8,7 @@ import { NavController } from 'ionic-angular';
 
 
 export class HomePage {
-  snippetArea = ""
-  snippetName = "untitled"
+
   constructor(public navCtrl: NavController) {}
 
-  public saveSnippet() {
-    console.log(this.snippetArea)
-    console.log(this.snippetName)
-
-    let data = JSON.parse(localStorage.getItem('userData'))
-
-    if (data != undefined) {
-      data['snippets'].push({"name": this.snippetName, "snippet": this.snippetArea})
-      localStorage.setItem('userData', JSON.stringify(data))
-
-    } else {
-      data = {
-        "snippets": [{
-          "name": this.snippetName,
-          "snippet": this.snippetArea
-        }]
-      }
-
-      localStorage.setItem('userData', JSON.stringify(data))
-    }
-  }
 }
