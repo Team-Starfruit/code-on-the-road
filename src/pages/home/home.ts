295cbab8c6ce18cc7e02cfa6048161b35b889ed8
Terry Palomares <tpalomares18@gmail.com>
2017-11-05 17:16:16 -1000

    Created home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SnippetPage } from '../snippet/snippet'
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {}
 
+  createSnippet() {
+  	this.navCtrl.push(SnippetPage)
+  }
 }
